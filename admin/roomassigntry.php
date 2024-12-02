7359411587f704ec1cb76fd75eed2bc4314493cd
Faculty Loading <cosemichelle2@gmail.com>
2024-12-03 02:33:28 +0800

    Update roomassigntry.php

--- admin/roomassigntry.php
+++ admin/roomassigntry.php
@@ -212,7 +212,7 @@ $stmt->close();
                     <!-- Days Field -->
                     <div class="form-group">
                         <label for="edit_days" class="control-label">Days</label>
-                        <select class="form-control" name="days" id="edit_days" readonly>
+                        <select class="form-control" name="days" id="edit_days" required>
                             <option value="0" disabled selected>Select Days</option>
                             <?php
                             $stmt = $conn->prepare("SELECT * FROM days");
@@ -232,7 +232,7 @@ $stmt->close();
                     <!-- Timeslot Field -->
                     <div class="form-group">
                         <label for="edit_timeslot" class="control-label">Timeslot</label>
-                        <select class="form-control" name="timeslot" id="edit_timeslot" readonly>
+                        <select class="form-control" name="timeslot" id="edit_timeslot" required>
                             <option value="0" disabled selected>Select Timeslot</option>
                             <?php
                             $stmt = $conn->prepare("SELECT * FROM timeslot");
