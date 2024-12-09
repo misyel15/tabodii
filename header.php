024535358ce34832357581af2b06360a05b358d6
Faculty Loading <cosemichelle2@gmail.com>
2024-12-10 05:36:19 +0800

    Update header.php

--- header.php
+++ header.php
@@ -4,7 +4,8 @@
   
 
   <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Times+New+Roman&display=swap" rel="stylesheet">
+
     <link rel="stylesheet" href="admin/assets/font-awesome/css/all.min.css">
 
     <!-- Title Page-->
