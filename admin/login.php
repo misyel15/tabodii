862d928a5de97925f71b3768b297e67b85052973
Faculty Loading <cosemichelle2@gmail.com>
2024-12-09 19:11:07 +0800

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -290,7 +290,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <!-- Custom CSS for Modal Width -->
 <style>
 .custom-modal-dialog {
-    max-width: 50%; /* Adjust width as needed */
+    max-width: 20%; /* Adjust width as needed */
 }
 
 .custom-modal {
