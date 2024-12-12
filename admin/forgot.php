164afbbcd2bc6b1bc8e38d2478ed5cdd55d5d095
Faculty Loading <cosemichelle2@gmail.com>
2024-12-13 07:44:40 +0800

    Update forgot.php

--- admin/forgot.php
+++ admin/forgot.php
@@ -23,12 +23,12 @@ function sendemail($email, $reset_token)
     try {
         //Server settings
         $mail->isSMTP();
-        $mail->Host = 'smtp.gmail.com';
+        $mail->Host = 'smtp.office365.com';
         $mail->SMTPAuth = true;
         $mail->Username = 'zeninmacky05@gmail.com'; // SMTP username
         $mail->Password = 'frut mage zsxu mzsd';    // SMTP password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-        $mail->Port = 465;
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587;
 
         //Recipients
         $mail->setFrom('mccschedsystem@gmail.com', 'MCC SCHED SYSTEM ADMIN');
