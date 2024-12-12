21633a88f055e0659e44beac85dcab6f1308f01a
Faculty Loading <cosemichelle2@gmail.com>
2024-12-12 12:32:45 +0800

    Update form.php

--- admin/form.php
+++ admin/form.php
@@ -70,7 +70,7 @@ include 'includes/head.php';
         }
 
         .card-header {
-            background-color: red;
+            background-color: #CB4154;
             color: black;
             text-align: center;
             padding: 1.5rem;
