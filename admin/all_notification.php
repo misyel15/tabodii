37ec4bdd1a2f82d2bbecf41efa20ad4bfcca0650
Faculty Loading <cosemichelle2@gmail.com>
2024-12-12 13:10:35 +0800

    Update all_notification.php

--- admin/all_notification.php
+++ admin/all_notification.php
@@ -34,7 +34,7 @@ $currentTime = date('d-m-Y h:i:s A', time());
                 <div class="row justify-content-center">
                     <div class="col-lg-6">
                         <div class="au-card au-card--no-shadow au-card--no-pad m-b-40">
-                            <div class="au-card-title" style="background-image:url('<?php echo htmlentities($data['Image']); ?>'); background-color: #8B0000;">
+                            <div class="au-card-title" style="background-image:url('<?php echo htmlentities($data['Image']); ?>');  background-color: #8B0000;">
                              
                                 <?php
                                 // Fetch unread notifications count
