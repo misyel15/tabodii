9b48108506e8ee84f421e912891f83bd9228ea1d
Faculty Loading <cosemichelle2@gmail.com>
2024-12-12 13:00:37 +0800

    Update view_notif.php

--- admin/view_notif.php
+++ admin/view_notif.php
@@ -32,19 +32,24 @@ $currentTime = date('d-m-Y h:i:s A', time());
         <br><br><br>
         <div class="content-wrapper">
             <br>
-            <section class="container-fluid">
-                <div class="row justify-content-center">
-                    <div class="col-lg-6">
-                        <div class="au-card au-card--no-shadow au-card--no-pad m-b-40">
-                            <div class="au-card-title" style="background-image:url('<?php echo isset($data['Image']) ? htmlentities($data['Image']) : ''; ?>');">
-                                <div class="bg-overlay bg-overlay--blue"></div>
-                                <h3>
-                                    <button class="btn-sm" style="margin-right: 1px;" onclick="location.href='home.php'">
-                                        <i class="fa fa-arrow-circle-left"></i>
-                                    </button>
-                                    <i class="zmdi zmdi-notifications"></i> Notifications
-                                </h3>
-                            </div>
+           <section class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="col-lg-6">
+            <div class="au-card au-card--no-shadow au-card--no-pad m-b-40">
+                <div class="au-card-title" style="background-image:url('<?php echo isset($data['Image']) ? htmlentities($data['Image']) : ''; ?>');">
+                    <div class="bg-overlay bg-overlay--red"></div> <!-- Updated class -->
+                    <h3>
+                        <button class="btn-sm" style="margin-right: 1px;" onclick="location.href='home.php'">
+                            <i class="fa fa-arrow-circle-left"></i>
+                        </button>
+                        <i class="zmdi zmdi-notifications"></i> Notifications
+                    </h3>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
 
                             <?php
                             // Fetch notifications from the database
