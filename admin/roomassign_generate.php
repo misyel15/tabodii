fc879bbb80fb3c0d064bdaa7a2fda20cd6b5c982
Faculty Loading <cosemichelle2@gmail.com>
2024-12-19 06:45:08 +0800

    Update roomassign_generate.php

--- admin/roomassign_generate.php
+++ admin/roomassign_generate.php
@@ -164,12 +164,12 @@ function printPage($conn, $dept_id) {
         <img src="<?php echo htmlspecialchars($headerImage); ?>" alt="Department Header">
     </div>
         <?php echo $content; ?>
-     <script>
+  <script>
             // Detect when the print dialog is closed
-            window.onafterprint = function() {
+         //   window.onafterprint = function() {
                 // Redirect back if the print dialog was canceled
-                window.history.back();
-            };
+            //    window.history.back();
+          //  };
         </script>
     </body>
     </html>
